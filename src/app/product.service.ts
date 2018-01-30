b83186b42d1d5518459790c1d02a7de35646e787
Craig Spreeman <craigspreeman@gmail.com>
2018-01-29 16:30:20 -0800

    module 3 part 1

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/map';
 export class ProductService {
 
   constructor(private _http:Http) { }
-  private _albumUrl:string = '../assets/album.json'
+  private _albumUrl:string = '../assets/album.json';
 
   getAlbum(id:number) {
     return this._http.get(this._albumUrl).map(Response => { 
